75a9dad3d01fef8aec972d5793ddd697d25d073b
Zenichi Amano <crow.misia@gmail.com>
2025-01-04 10:58:51 +0900

    build: dokka v2対応
    gradleのconfiguration cache対応
    
    Signed-off-by: Zenichi Amano <crow.misia@gmail.com>

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 import java.net.URI
@@ -6,6 +5,7 @@ import java.net.URI
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.dokka)
+    alias(libs.plugins.dokka.javadoc)
     alias(libs.plugins.kotlinx.kover)
     alias(libs.plugins.detekt)
     id("maven-publish")
@@ -33,106 +33,92 @@ dependencies {
     compileOnly(platform(libs.kotlin.bom))
     compileOnly(libs.kotlin.stdlib)
 
-    testImplementation(libs.mockk)
+    testImplementation(platform(libs.kotest.bom))
     testImplementation(libs.kotest.runner.junit5)
     testImplementation(libs.kotest.assertions.core)
     testImplementation(libs.kotest.property)
+    testImplementation(libs.mockk)
 }
 
-val sourcesJar by tasks.creating(Jar::class) {
+val sourcesJar by tasks.registering(Jar::class) {
     group = JavaBasePlugin.DOCUMENTATION_GROUP
     description = "Assembles sources JAR"
-    archiveClassifier.set("sources")
-    from(sourceSets.getByName("main").allSource)
+    archiveClassifier = "sources"
+    from(sourceSets.main.get().allSource)
 }
 
-val customDokkaTask by tasks.creating(DokkaTask::class) {
-    dokkaSourceSets.getByName("main") {
-        noAndroidSdkLink.set(false)
-    }
-    dependencies {
-        plugins(libs.dokka.javadoc.plugin)
-    }
-    inputs.dir("src/main/kotlin")
-    outputDirectory.set(layout.buildDirectory.dir("javadoc"))
+val dokkaJavadocJar by tasks.registering(Jar::class) {
+    description = "A Javadoc JAR containing Dokka Javadoc"
+    archiveClassifier = "javadoc"
+    from(tasks.dokkaGeneratePublicationJavadoc.flatMap { it.outputDirectory })
 }
 
-val javadocJar by tasks.creating(Jar::class) {
-    dependsOn(customDokkaTask)
-    group = JavaBasePlugin.DOCUMENTATION_GROUP
-    description = "Assembles JavaDoc JAR"
-    archiveClassifier.set("javadoc")
-    from(customDokkaTask.outputDirectory)
-}
+publishing {
+    publications {
+        register<MavenPublication>("maven") {
+            groupId = Maven.groupId
+            artifactId = Maven.artifactId
 
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("maven") {
-                groupId = Maven.groupId
-                artifactId = Maven.artifactId
+            println("""
+                |Creating maven publication
+                |    Group: $groupId
+                |    Artifact: $artifactId
+                |    Version: $version
+            """.trimMargin())
 
-                println("""
-                    |Creating maven publication
-                    |    Group: $groupId
-                    |    Artifact: $artifactId
-                    |    Version: $version
-                """.trimMargin())
+            artifact(sourcesJar)
+            artifact(dokkaJavadocJar)
+            from(components["kotlin"])
 
-                artifact(sourcesJar)
-                artifact(javadocJar)
-                from(components["kotlin"])
+            pom {
+                name = Maven.name
+                description = Maven.desc
+                url = Maven.siteUrl
 
-                pom {
-                    name.set(Maven.name)
-                    description.set(Maven.desc)
-                    url.set(Maven.siteUrl)
+                scm {
+                    val scmUrl = "scm:git:${Maven.gitUrl}"
+                    connection = scmUrl
+                    developerConnection = scmUrl
+                    url = Maven.gitUrl
+                    tag = "HEAD"
+                }
 
-                    scm {
-                        val scmUrl = "scm:git:${Maven.gitUrl}"
-                        connection.set(scmUrl)
-                        developerConnection.set(scmUrl)
-                        url.set(Maven.gitUrl)
-                        tag.set("HEAD")
+                developers {
+                    developer {
+                        id = "crow-misia"
+                        name = "Zenichi Amano"
+                        email = "crow.misia@gmail.com"
+                        roles = listOf("Project-Administrator", "Developer")
+                        timezone = "+9"
                     }
+                }
 
-                    developers {
-                        developer {
-                            id.set("crow-misia")
-                            name.set("Zenichi Amano")
-                            email.set("crow.misia@gmail.com")
-                            roles.set(listOf("Project-Administrator", "Developer"))
-                            timezone.set("+9")
-                        }
-                    }
-
-                    licenses {
-                        license {
-                            name.set(Maven.licenseName)
-                            url.set(Maven.licenseUrl)
-                            distribution.set(Maven.licenseDist)
-                        }
+                licenses {
+                    license {
+                        name = Maven.licenseName
+                        url = Maven.licenseUrl
+                        distribution = Maven.licenseDist
                     }
                 }
             }
         }
-        repositories {
-            maven {
-                val releasesRepoUrl = URI("https://oss.sonatype.org/service/local/staging/deploy/maven2")
-                val snapshotsRepoUrl = URI("https://oss.sonatype.org/content/repositories/snapshots")
-                url = if (Maven.version.endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-                credentials {
-                    username = providers.gradleProperty("sona.user").orElse(providers.environmentVariable("SONA_USER")).orNull
-                    password = providers.gradleProperty("sona.password").orElse(providers.environmentVariable("SONA_PASSWORD")).orNull
-                }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = URI("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+            val snapshotsRepoUrl = URI("https://oss.sonatype.org/content/repositories/snapshots")
+            url = if (Maven.version.endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            credentials {
+                username = providers.gradleProperty("sona.user").orElse(providers.environmentVariable("SONA_USER")).orNull
+                password = providers.gradleProperty("sona.password").orElse(providers.environmentVariable("SONA_PASSWORD")).orNull
             }
         }
     }
+}
 
-    signing {
-        useGpgCmd()
-        sign(publishing.publications.getByName("maven"))
-    }
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
 }
 
 detekt {
@@ -140,21 +126,21 @@ detekt {
     buildUponDefaultConfig = true
     allRules = false
     autoCorrect = true
-    config.setFrom(files("$rootDir/config/detekt.yml"))
+    config.from(rootDir.resolve("config/detekt.yml"))
 }
 
 java {
     toolchain {
-        setSourceCompatibility(JavaLanguageVersion.of(8))
-        setTargetCompatibility(JavaLanguageVersion.of(8))
-        languageVersion.set(JavaLanguageVersion.of(23))
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+        languageVersion = JavaLanguageVersion.of(23)
     }
 }
 
 kotlin {
     compilerOptions {
         freeCompilerArgs.add("-Xconsistent-data-class-copy-visibility")
-        javaParameters.set(true)
+        javaParameters = true
         jvmTarget = JvmTarget.JVM_1_8
         apiVersion = KotlinVersion.KOTLIN_2_0
         languageVersion = KotlinVersion.KOTLIN_2_0
@@ -164,25 +150,25 @@ kotlin {
 
 tasks {
     withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
-        jvmTarget = "19"
+        jvmTarget = "1.8"
         reports {
-            html.required.set(false)
-            xml.required.set(false)
-            txt.required.set(false)
-            sarif.required.set(true)
-            md.required.set(true)
+            html.required = false
+            xml.required = false
+            txt.required = false
+            sarif.required = true
+            md.required = true
         }
         exclude("build/")
         exclude("resources/")
     }
 
     withType<io.gitlab.arturbosch.detekt.DetektCreateBaselineTask>().configureEach {
-        jvmTarget = "19"
+        jvmTarget = "1.8"
         exclude("build/")
         exclude("resources/")
     }
 
-    withType<Test> {
+    withType<Test>().configureEach {
         useJUnitPlatform()
         testLogging {
             showStandardStreams = true
